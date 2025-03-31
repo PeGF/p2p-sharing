4a6b0af3b06d523afdea59a6357a210f7ca7e752
Oliver Kuramae <oliverkuramae@usp.br>
2025-03-31 20:42:48 -0300

    Remove run.sh

--- run1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Change to the directory containing the script
-cd /home/oliver/Docs/usp/p2p-sharing
-
-# Run the Python script with the required arguments
-python3 eachare.py 127.0.0.1:5000 vizinhos.txt diretorio_compartilhado
